ba9d46a9cb84f302f89e88969b523362b4c081d6
Jatin1628 <jatinnainwani24@gmail.com>
2025-09-24 23:38:45 +0530

    upload

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { ChangeEvent } from "react";
 
 interface FileUploadProps {
@@ -13,14 +14,16 @@ const FileUpload: React.FC<FileUploadProps> = ({ onFileChange }) => {
   };
 
   return (
-    <div className="flex items-center justify-center w-full">
+  <div>
+      <div className="flex flex-col gap-5 items-center justify-center w-full mt-10">
+        <h1 className="font-bold text-4xl">Upload files to encrypt</h1>
       <label
         htmlFor="dropzone-file"
-        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        className="flex flex-col items-center justify-center w-1/2 h-64 border-2 border-white border-dashed rounded-lg cursor-pointer bg-black hover:bg-gray-900"
       >
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
           <svg
-            className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+            className="w-8 h-8 mb-4 text-gray-300"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -34,11 +37,11 @@ const FileUpload: React.FC<FileUploadProps> = ({ onFileChange }) => {
               d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
             />
           </svg>
-          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+          <p className="mb-2 text-sm text-gray-300">
             <span className="font-semibold">Click to upload</span> or drag and
             drop
           </p>
-          <p className="text-xs text-gray-500 dark:text-gray-400">
+          <p className="text-xs text-gray-400">
             SVG, PNG, JPG or GIF (MAX. 800x400px)
           </p>
         </div>
@@ -51,6 +54,7 @@ const FileUpload: React.FC<FileUploadProps> = ({ onFileChange }) => {
         />
       </label>
     </div>
+  </div>
   );
 };
 
